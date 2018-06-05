1f710458aff181684469413f4ed91722460df84a
Hoanh Tung <hoanhtung.xy@gmail.com>
2018-06-05 23:23:19 +0700

    add field for exam and answer

--- db/migrate/20180604155406_create_answers.rb
+++ db/migrate/20180604155406_create_answers.rb
@@ -2,8 +2,9 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
       t.text :content
+      t.boolean :is_right
       t.integer :question_id
-
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_155432) do
 
   create_table "answers", force: :cascade do |t|
     t.text "content"
+    t.boolean "is_right"
     t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
