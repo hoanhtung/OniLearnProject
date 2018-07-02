231ede8e6fb01a852534dd378e9f01b04a56c956
hoanhtung <hoanhtung.xy@gmail.com>
2018-07-02 17:43:31 +0700

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    # before_action :authenticate_user!
+    before_action :authenticate_user!
     
+    def after_sign_in_path_for(resource_or_scope)
+        home_path
+    end
+
+    def after_sign_up_path_for(resource)
+        after_sign_in_path_for(resource)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,35 @@
 Rails.application.routes.draw do
-  root to: "welcome#home"
-  # namespace :api do
-    devise_for :users, :controllers => {
-      :omniauth_callbacks => "user/omniauth_callbacks"
-    }
-
-    get '/find_by_category_id', to: 'subjects#find_all_by_cate_id'
-    get '/find_by_subject_id', to: 'courses#find_all_by_sub_id'
-    resources :exams
-    resources :answer_details
-    resources :exam_details
-  # resources :users
-  
-  resources :categories, only: [:index, :edit, :update, :new, :create] , shallow: true do
-    resources :subjects, shallow: true do
-      resources :courses, shallow: true do
-        resources :questions do
-          resources :answers
-        end
+  # root to: "welcome#home"
+  # namespace :api, defaults: { format: :json } do
+  #   namespace :v1 do
+      devise_for :users, :controllers => {
+        :omniauth_callbacks => "user/omniauth_callbacks",
+        :sessions => "user/sessions",
+        :registrations => "user/registrations"
+      }
+      devise_scope :user do
+        root to: "user/sessions#new"
       end
-    end
+
+      get '/home', to: 'welcome#home'
+
+      get '/find_by_category_id', to: 'subjects#find_all_by_cate_id'
+      get '/find_by_subject_id', to: 'courses#find_all_by_sub_id'
+      # resources :exams
+      # resources :answer_details
+      # resources :exam_details
+    # resources :users
+    
+      resources :categories, only: [:index, :edit, :update, :new, :create] , shallow: true do
+        resources :subjects, shallow: true do
+          resources :courses, shallow: true do
+            resources :questions do
+              resources :answers
+            end
+          end
+        end
+    #   end
+    # end
   end
-# end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
