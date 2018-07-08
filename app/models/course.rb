eaed4a68396c9bcdfbb87a03a5802928cfd34b0b
Hoanh Tung <hoanhtung.xy@gmail.com>
2018-07-08 17:03:36 +0700

    add icon for course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,12 @@
 class Course < ApplicationRecord
     has_many :questions
     belongs_to :subject
+
+    mount_uploader :icon, ImageUploader
+    validates_processing_of :icon
+
+    private
+    def image_size_validation
+        errors[:icon] << "should be less than 500KB" if image.size > 0.5.megabytes
+    end
 end
