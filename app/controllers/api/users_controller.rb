fec1f582eaaa8881f89456d5834406dac704bac3
Hoanh Tung <hoanhtung.xy@gmail.com>
2018-07-22 22:34:22 +0700

    fix api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -32,9 +32,9 @@ class API::UsersController < ApplicationController
   def create
     @user = ::User.new(user_params)
     if @user.save
-      render json: @user
+      render json: @user, status: :created
     else
-      render json: @user.errors
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
