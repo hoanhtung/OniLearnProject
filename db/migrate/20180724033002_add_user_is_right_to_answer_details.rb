f65f9a47d3df7da65475fcb28ebf77bb38206ec2
Hoanh Tung <hoanhtung.xy@gmail.com>
2018-07-24 10:59:58 +0700

    fix migrate

--- db/migrate/20180724033002_add_user_is_right_to_answer_details.rb
+++ db/migrate/20180724033002_add_user_is_right_to_answer_details.rb
@@ -1,5 +1,5 @@
 class AddUserIsRightToAnswerDetails < ActiveRecord::Migration[5.2]
   def change
-    add_column :answer_details, :user_id_right, :boolean
+    rename_column :answer_details, :user_id_right, :user_is_right
   end
 end
